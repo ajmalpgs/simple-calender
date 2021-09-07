af37e725e2e79b205b9104b12f5df3fb6cbfdb65
ajmalpgs <ajmal@greensmedia.com>
2021-09-07 23:18:54 +0530

    first commit

---
+++ ajax.php
@@ -0,0 +1,5 @@
+<?php 
+ 
+echo json_encode([['startDate'=>'Wed Oct 6 2021','endDate'=>'Wed Oct 6 2021','summary'=>'ajmal test'],['startDate'=>'Wed Oct 13 2021','endDate'=>'Wed Oct 13 2021','summary'=>'ajmal test']]);
+
+?>
